728931bc3534a9928fbb66ea97a313fd40f819ef
Gustavo Cysneiros <gcc.prof@protonmail.com>
2023-10-22 22:43:31 -0300

    Fetch data with useEffect - bug fix

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
 
     return (
         <div className=" home">
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}></BlogList>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"></BlogList>}
         </div>
     );
 }
