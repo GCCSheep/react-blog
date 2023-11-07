b473b4049ec40dfa0b9d0cd887bbc82e731c0e6d
Gustavo Cysneiros <gcc.prof@protonmail.com>
2023-11-07 13:21:26 -0300

    Programmatic redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
-
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
-    const [isPending, setIsPending] = useState('false');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,6 +18,7 @@ const Create = () => {
         }).then(() => {
             console.log('New blog added.');
             setIsPending(false);
+            history.push('/');
         });
     }
 
@@ -41,7 +43,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
             </form>
         </div>
     );
